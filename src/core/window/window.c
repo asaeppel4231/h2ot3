d3b494622c08ebdbf6ce2c4754469e3ee1f4712e
asaeppel4231 <asaeppel4231@asaeppel.ddns.net>
2026-02-20 11:04:04 +0100

    Add CMake File, add testcode, implement creation of a h2ot3 window

--- src/core/window/window.c
+++ src/core/window/window.c
@@ -16,7 +16,11 @@ struct h2ot3_window {
 h2ot3_window_t* new_h2ot3_window(uint width_px, uint height_px, uint edge_radius, int opts){
     h2ot3_window_t* window = malloc(sizeof(h2ot3_window_t));
     RETURN_IF_NULL(window);
-
+    window->width_px = width_px;
+    window->height_px = height_px;
+    window->edge_radius = edge_radius;
+    window->child_objects = NULL;
+    return window;
 }
 
 void            free_h2ot3_window(h2ot3_window_t* window){
--- src/h2ot3.c
+++ src/h2ot3.c
@@ -1,5 +1,7 @@
 #include "h2ot3.h"
+#include "window/window.h"
 
 int main(){
-    
+    h2ot3_window_t* window = new_h2ot3_window(180, 240, 5, 0);
+    free_h2ot3_window(window);    
 }
