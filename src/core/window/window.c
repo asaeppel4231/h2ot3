5d1c99ef973475d86fb9a960b0b908b4ab1d8c52
asaeppel4231 <asaeppel4231@asaeppel.ddns.net>
2026-02-23 18:38:46 +0100

    First fix in window.c with the invalid macro

--- src/core/window/window.c
+++ src/core/window/window.c
@@ -6,16 +6,19 @@
 #include "_internal/_internal.h"
 #include "types.h"
 
+#define H2OT3_INVALID 1 /*only for the first*/ /*TODO: Move that to anything else*/ 
+
 struct h2ot3_window {
     char* title;
     uint width_px;
     uint height_px;
+    uint visible:1;
     void** child_objects;
 };
 
 h2ot3_window_t* new_h2ot3_window(char* title, uint width_px, uint height_px, int opts){
     h2ot3_window_t* window = malloc(sizeof(h2ot3_window_t));
-    RETURN_IF_NULL(window);
+    RETURN_IF_NULL(window, NULL);
     window->title = _internal_deep_copy(title, MAX_WINDOW_TITLE_LEN);
     window->width_px = width_px;
     window->height_px = height_px;
@@ -32,37 +35,37 @@ void            free_h2ot3_window(h2ot3_window_t* window){
 }
 
 char*           get_h2ot3_window_title(h2ot3_window_t* window){
-    RETURN_IF_NULL(window);
+    RETURN_IF_NULL(window, NULL);
     return window->title;
 }
 
 uint            get_h2ot3_window_width_px(h2ot3_window_t* window){
-    RETURN_IF_NULL(window); // FIXME: Returns void* instead of uint if invalid
+    RETURN_IF_NULL(window, H2OT3_INVALID);
     return window->width_px;
 }
 
 uint            get_h2ot3_window_heigh_px(h2ot3_window_t* window){
-    RETURN_IF_NULL(window);
+    RETURN_IF_NULL(window, H2OT3_INVALID);
     return window->height_px;
 }
 
 uint            set_h2ot3_window_title    (h2ot3_window_t* window, char* title){
     if(window == NULL){
-        return -1; // FIXME: Use another value
+        return H2OT3_INVALID;
     }
     window->title = title;
 }
 
 uint            set_h2ot3_window_width_px (h2ot3_window_t* window, uint width_px){
     if(window == NULL){
-        return -1;
+        return H2OT3_INVALID;
     }
     window->width_px = width_px;
 }
 
 uint            set_h2ot3_window_height_px(h2ot3_window_t* window, uint height_px){
     if(window == NULL){
-        return -1;
+        return H2OT3_INVALID;
     }
     window->height_px = height_px;
 }
