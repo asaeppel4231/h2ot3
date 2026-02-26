d3610c8e48bda5b601db8d47d73389ab54c1c98f
asaeppel4231 <asaeppel4231@asaeppel.ddns.net>
2026-02-26 15:31:18 +0100

    Add helper in internal container module and update CMake File (update source file list and include file list)

--- src/core/container/_internal_container.c
+++ src/core/container/_internal_container.c
@@ -14,6 +14,15 @@ struct h2ot3_container{
     h2ot3_window_t* window;
 };
 
+void _internal_helper_h2ot3_set_delete_flag(uint flag, uint bit, uint set_delete){
+    if(mbool_is_true(set_delete)){
+        flag |=  (1 << bit);   // Set bit
+    }
+    else{
+        flag &= ~(1 << bit);   // Remove bit
+    }
+}
+
 uint  _internal_h2ot3_container_get_flags_raw     (h2ot3_container_t* container){
     return container->flags;
 }
@@ -30,28 +39,13 @@ mbool _internal_h2ot3_container_get_needs_relayout(h2ot3_container_t* container)
 }
 
 void _internal_h2ot3_container_set_visibility    (h2ot3_container_t* container, mbool visible){
-    if (visible){
-        container->flags |=  (1 << H2OT3_CONTAINER_VISIBILITY_BIT);   // Set bit
-    }
-    else{
-        container->flags &= ~(1 << H2OT3_CONTAINER_VISIBILITY_BIT);   // Remove bit
-    }
+    _internal_helper_h2ot3_set_delete_flag(container->flags, H2OT3_CONTAINER_VISIBILITY_BIT, visible);
 }
 
 void _internal_h2ot3_container_set_needs_redraw  (h2ot3_container_t* container, mbool needs_redraw){
-    if (needs_redraw){
-        container->flags |=  (1 << H2OT3_CONTAINER_REDRAW_BIT);   // Set bit
-    }
-    else{
-        container->flags &= ~(1 << H2OT3_CONTAINER_REDRAW_BIT);   // Remove bit
-    }
+    _internal_helper_h2ot3_set_delete_flag(container->flags, H2OT3_CONTAINER_REDRAW_BIT, needs_redraw);
 }
 
 void _internal_h2ot3_container_set_needs_relayout(h2ot3_container_t* container, mbool needs_relayout){
-    if (needs_relayout){
-        container->flags |=  (1 << H2OT3_CONTAINER_RELAYOUT_BIT);   // Set bit
-    }
-    else{
-        container->flags &= ~(1 << H2OT3_CONTAINER_RELAYOUT_BIT);   // Remove bit
-    }
+    _internal_helper_h2ot3_set_delete_flag(container->flags, H2OT3_CONTAINER_RELAYOUT_BIT, needs_relayout);
 }
